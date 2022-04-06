ac25890a0de68361adddac6327b5a9bfadfe618e
Jeremie Cambakidis <jcambaki@z3r3p5.42lyon.fr>
2022-04-06 17:35:48 +0200

    Fix JOIN command

--- sources/commands/JoinCommand.cpp
+++ sources/commands/JoinCommand.cpp
@@ -19,6 +19,7 @@ bool	JoinCommand::execute(User *commandSender, std::vector<std::string> args)
 		return false;
 	}
 
+
 	channels = parseArg(args.at(1));
 	if (channels.size() > 2)
 	{
@@ -37,34 +38,50 @@ bool	JoinCommand::execute(User *commandSender, std::vector<std::string> args)
 	while (i < channels.size())
 	{
 		Channel *ch = getServer()->getChannel(channels.at(i));
-		if (ch == NULL)
-			commandSender->sendMessage(NULL, "Channel not found");
-		else if (ch->getUser(commandSender->getNickname()) == commandSender)
-			commandSender->sendMessage(NULL, "You are already in this channel");
-		else if (ch->getBanUser(commandSender->getNickname()) == commandSender)
-			commandSender->sendMessage(NULL, "You are banned from this channel");
-		else if (ch->getInvitedUser(commandSender->getNickname()) != commandSender && ch->isInviteOnly())
-			commandSender->sendMessage(NULL, "You must be invited to join this channel");
-		else if (ch->getUsers().size() == ch->getMaxSize())
-			commandSender->sendMessage(NULL, "The target channel is full");
-		else if ((keys.size() > 0 && ch->getPassword() != keys.at(i))
-		|| (keys.size() == 0 && ch->getPassword() != ""))
-			commandSender->sendMessage(NULL, "Wrong channel key");
+
+		if (checkBadCharacters(channels.at(i)))
+			commandSender->sendMessage(NULL, "Bad characters");
 		else
 		{
-			std::string tmp = ch->getName();
-			tmp.append(" :");
-			tmp.append(ch->getTopic());
-			commandSender->sendMessage(NULL, tmp);
-			tmp.clear();
+			if (ch == NULL)
+			{
+				try
+				{
+					getServer()->createChannel(channels.at(i), keys.at(i), false, 10);
+				}
+				catch(const std::exception& e)
+				{
+					getServer()->createChannel(channels.at(i), "", false, 10);
+				}
+				ch = getServer()->getChannel(channels.at(i));
+			}
+			if (ch->getUser(commandSender->getNickname()) == commandSender)
+				commandSender->sendMessage(NULL, "You are already in this channel");
+			else if (ch->getBanUser(commandSender->getNickname()) == commandSender)
+				commandSender->sendMessage(NULL, "You are banned from this channel");
+			else if (ch->getInvitedUser(commandSender->getNickname()) != commandSender && ch->isInviteOnly())
+				commandSender->sendMessage(NULL, "You must be invited to join this channel");
+			else if (ch->getUsers().size() == ch->getMaxSize())
+				commandSender->sendMessage(NULL, "The target channel is full");
+			else if ((keys.size() > i && ch->getPassword() != keys.at(i))
+			|| (keys.size() == 0 && ch->getPassword() != ""))
+				commandSender->sendMessage(NULL, "Wrong channel key");
+			else
+			{
+				std::string tmp = ch->getName();
+				tmp.append(" :");
+				tmp.append(ch->getTopic());
+				commandSender->sendMessage(NULL, tmp);
+				tmp.clear();
 
-			ch->removeInvitedUser(commandSender);
+				ch->removeInvitedUser(commandSender);
 
-			tmp = commandSender->getNickname();
-			tmp.append(" has joined the channel :").append(ch->getName()).append(".");
-			ch->sendMessage(NULL, tmp);
-			ch->addUser(commandSender);
-			commandSender->addChannel(ch);
+				tmp = commandSender->getNickname();
+				tmp.append(" has joined the channel :").append(ch->getName()).append(".");
+				ch->sendMessage(NULL, tmp);
+				ch->addUser(commandSender);
+				commandSender->addChannel(ch);
+			}
 		}
 		i++;
 	}
--- sources/commands/PrivMsgCommand.cpp
+++ sources/commands/PrivMsgCommand.cpp
@@ -39,37 +39,5 @@ bool	PrivMsgCommand::execute(User *commandSender, std::vector<std::string> args)
 		else
 			commandSender->sendMessage(NULL, "Wrong arguments");
 	}
-	
-/*
-
-	std::vector<std::string> targetChannels = getUsersNameInString(args.at(1));
-
-	std::string message = args.at(2);
-	for (size_t i = 3; i < args.size(); i++)
-		message.append(" ").append(args.at(i));
-	message.erase(message.begin());
-
-	for (size_t i = 0; i < targetUsers.size(); i++)
-	{
-		User *targetUser = getServer()->getUser(targetUsers.at(i));
-		if (targetUser == NULL)
-		{
-			commandSender->sendMessage(NULL, std::string("Can't find user: ") + targetUsers.at(i));
-			continue;
-		}
-		targetUser->sendMessage(commandSender, message);
-	}
-
-	for (size_t i = 0; i < targetChannels.size(); i++)
-	{
-		Channel *targetChannel = getServer()->getChannel(targetChannels.at(i));
-		if (targetChannel == NULL)
-		{
-			commandSender->sendMessage(NULL, std::string("Can't find channel: ") + targetChannels.at(i));
-			continue;
-		}
-		targetChannel->sendMessage(commandSender, message);
-	}
-*/
 	return true;
 }
